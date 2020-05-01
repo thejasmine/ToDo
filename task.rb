fc5682be619e2856a72ebe95aa99c423dd946dc7
Jasmine Huang <tinjasminee@gmail.com>
2020-05-01 09:47:45 +0800

    create title

---
+++ task.rb
@@ -0,0 +1,8 @@
+class Task
+  def initialize(title)
+    @title = title
+    @done = false
+  end
+
+  attr-reader :tittle
+end
--- view.rb
+++ view.rb
@@ -2,4 +2,12 @@ class View
   def initialize
 
   end
+
+  def display_tasks(array_of_tasks)
+    #get the tasks from the repo
+    tasks
+    #iterate over the array of tasks
+    #interpolate each task into a nich menu
+  end
+
 end
