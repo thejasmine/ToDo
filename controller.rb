2091e22b05a12e718f35816929ed46253820d78b
Jasmine Huang <tinjasminee@gmail.com>
2020-05-02 11:34:30 +0800

    create all files

---
+++ controller.rb
@@ -0,0 +1,13 @@
+class Controller
+  def initialize
+    @view
+  end
+
+  def create_task
+    #ask user what they want to do
+    title =
+    #create a task instance
+    task =
+    #store the taks
+  end
+end
--- view.rb
+++ view.rb
@@ -5,9 +5,17 @@ class View
 
   def display_tasks(array_of_tasks)
     #get the tasks from the repo
-    tasks
+    tasks.each_with_index do |task, index|
+      puts "#{ index + 1} - #{task.title}"
+    end
     #iterate over the array of tasks
     #interpolate each task into a nich menu
+  end
+
+  def ask_user_for_title
+    puts "What's the taks?"
+    input = gets.chomp
+    return input
   end
 
 end
